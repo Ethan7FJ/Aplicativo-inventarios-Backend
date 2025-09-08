49ee55049bdae38a3fb2a8110df6c71f2df7eb67
Ethan7FJ <johanfetecua11@gmail.com>
2025-09-08 14:54:07 -0500

    Ruta para el inicio de sesion con su creacion y validacion del token, para el ingreso al dashboard, fianlizados

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,17 @@ import { Server } from "socket.io";
 import cors from "cors";
 import bcrypt from "bcryptjs";
 import { db } from "./db.js";
-/* import jwt from "jsonwebtoken"; */
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { verifyToken } from "./authMiddleware.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 
-const SECRET = '';
+dotenv.config();
+
+const SECRET = process.env.JWT_SECRET as string;
 
 /* Peticion de registro */
 
@@ -40,6 +44,41 @@ app.post("/registro", async (req, res) => {
   }
 });
 
+app.post("/inicio-sesion", async (req, res) => {
+  const { username, password } = req.body;
+
+  try{
+    const [rows]: any = await db.query(
+      "SELECT * FROM users WHERE username = ?",[username]
+    );
+    if(rows.length === 0){
+      return res.status(401).json({error: "El usuario no ha sido encontrado o no se encuentra registrado"})
+    }
+
+    const user = rows[0];
+
+    const Validar = await bcrypt.compare(password, user.password);
+    if(!Validar){
+      return res.status(401).json({error:"La contraseña ingresada no es la correcta"})
+    }
+
+    const token = jwt.sign(
+      {id: user.id, username:user.username},
+      SECRET,
+      {expiresIn: "1h"}
+    )
+
+    res.json({success: true, token})
+
+  }catch(err){
+    console.log("Error al iniciar sesion",err);
+    res.status(500).json({error: "Error en el servidor"})
+  }
+});
+
+app.get("/perfil", verifyToken, (req, res) => {
+  res.json({ success: true, user: (req as any).user });
+});
 
 
 const httpServer = createServer(app);
